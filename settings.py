6c439992586069ffaf22d906da76eb25370b2d4f
Matt Covalt <mcovalt@mailbox.org>
2019-08-21 23:02:23 -0400

    Clean up environment variables

--- settings.py
+++ settings.py
@@ -9,17 +9,8 @@ load_dotenv()
 class FlaskConfig(object):
     """Settings specific to Flask."""
 
-    ENV = os.getenv("FLASK_ENV")
-    if ENV not in ["production", "development", "testing"]:
-        raise ValueError(
-            'Invalid value for ENV: "'
-            + ENV
-            + '". Must be either "production", "development", or "testing".'
-        )
-    DEBUG = True if ENV in ["development", "testing"] else False
-    TESTING = True if ENV == "testing" else False
+    TESTING = os.getenv("FLASK_TESTING", False)
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
-    SERVER_NAME = os.getenv("FLASK_SERVER_NAME", None)
     APPLICATION_ROOT = os.getenv("FLASK_APPLICATION_ROOT", "/")
     PAGE_SIZE = 10000
 
@@ -27,15 +18,17 @@ class FlaskConfig(object):
 class PyMySQLConfig(object):
     """Settings specific to PyMySQL."""
 
-    HOST = os.getenv("FLASK_SQL_HOST")
-    USER = os.getenv("FLASK_SQL_USER")
-    PASSWORD = os.getenv("FLASK_SQL_PASSWORD")
-    DATABASE = os.getenv("FLASK_SQL_DATABASE")
+    HOST = os.getenv("SQL_HOST")
+    PORT = os.getenv("SQL_PORT")
+    USER = os.getenv("SQL_USER")
+    PASSWORD = os.getenv("SQL_PASSWORD")
+    DATABASE = os.getenv("SQL_DATABASE")
 
     def get_connection(self):
         """Produce a pymysql database connection using these settings."""
         return pymysql.connections.Connection(
             host=self.HOST,
+            port=self.PORT,
             user=self.USER,
             password=self.PASSWORD,
             database=self.DATABASE,
