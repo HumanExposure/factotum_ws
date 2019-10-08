628af4a3829a6553048f9232bb18b86d1f156a09
Matt Covalt <mcovalt@mailbox.org>
2019-10-08 03:14:01 -0400

    Add new view

--- app/api/filters.py
+++ app/api/filters.py
@@ -1,6 +1,7 @@
+from django.db.models import Prefetch
 from django_filters import rest_framework as filters
 
-from dashboard.models import PUC
+from dashboard import models
 
 
 class PUCFilter(filters.FilterSet):
@@ -14,5 +15,35 @@ class PUCFilter(filters.FilterSet):
         return queryset.dtxsid_filter(value).with_num_products()
 
     class Meta:
-        model = PUC
+        model = models.PUC
+        fields = []
+
+
+class ProductFilter(filters.FilterSet):
+    chemical = filters.CharFilter(
+        help_text="A chemical DTXSID to filter products against.",
+        method="dtxsid_filter",
+        initial="DTXSID6026296",
+    )
+
+    def dtxsid_filter(self, queryset, name, value):
+        queryset = queryset.prefetch_related(None)
+
+        return queryset.filter(
+            datadocument__extractedtext__rawchem__dsstox__sid=value
+        ).prefetch_related(
+            Prefetch("puc_set"),
+            Prefetch(
+                "datadocument_set__extractedtext__rawchem",
+                queryset=models.RawChem.objects.select_related(
+                    "ingredient",
+                    "dsstox",
+                    "extracted_text__data_document__document_type",
+                    "extracted_text__data_document__data_group__data_source",
+                ).filter(dsstox__sid=value),
+            ),
+        )
+
+    class Meta:
+        model = models.Product
         fields = []
--- app/api/serializers.py
+++ app/api/serializers.py
@@ -4,7 +4,7 @@ from dashboard import models
 
 
 class PUCSerializer(serializers.ModelSerializer):
-    puc_name = serializers.SerializerMethodField(
+    name = serializers.SerializerMethodField(
         read_only=True, help_text="full name of this PUC"
     )
     num_products = serializers.IntegerField(
@@ -12,14 +12,14 @@ class PUCSerializer(serializers.ModelSerializer):
         help_text="the number of distinct products associated with this PUC",
     )
 
-    def get_puc_name(self, obj) -> str:
+    def get_name(self, obj) -> str:
         return ": ".join(n for n in (obj.gen_cat, obj.prod_fam, obj.prod_type) if n)
 
     class Meta:
         model = models.PUC
         fields = [
             "id",
-            "puc_name",
+            "name",
             "gen_cat",
             "prod_fam",
             "prod_type",
@@ -32,3 +32,103 @@ class PUCSerializer(serializers.ModelSerializer):
             "prod_fam": {"help_text": "product family"},
             "prod_type": {"help_text": "product type"},
         }
+
+
+class ChemicalSerializer(serializers.ModelSerializer):
+    sid = serializers.SerializerMethodField(read_only=True)
+    name = serializers.SerializerMethodField(read_only=True)
+    cas = serializers.SerializerMethodField(read_only=True)
+    qa = serializers.SerializerMethodField(read_only=True)
+
+    def get_sid(self, obj) -> str:
+        if obj.dsstox is None:
+            return None
+        return obj.dsstox.sid
+
+    def get_name(self, obj) -> str:
+        if obj.dsstox is None:
+            return obj.raw_chem_name
+        return obj.dsstox.true_chemname
+
+    def get_cas(self, obj) -> str:
+        if obj.dsstox is None:
+            return obj.raw_cas
+        return obj.dsstox.true_cas
+
+    def get_qa(self, obj) -> bool:
+        return obj.dsstox is not None
+
+    class Meta:
+        model = models.RawChem
+        fields = ["id", "sid", "rid", "name", "cas", "qa"]
+
+
+class DataTypeSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source="title")
+
+    class Meta:
+        model = models.DocumentType
+        fields = ["name", "description"]
+
+
+class DataSourceSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source="title")
+
+    class Meta:
+        model = models.DataSource
+        fields = ["name", "url", "description"]
+
+
+class IngredientSerializer(ChemicalSerializer):
+    min_weight_fraction = serializers.SerializerMethodField(read_only=True)
+    max_weight_fraction = serializers.SerializerMethodField(read_only=True)
+    data_type = DataTypeSerializer(source="extracted_text.data_document.document_type")
+    source = DataSourceSerializer(
+        source="extracted_text.data_document.data_group.data_source"
+    )
+
+    def get_min_weight_fraction(self, obj) -> float:
+        try:
+            ing = obj.ingredient
+            if not ing.lower_wf_analysis and not ing.upper_wf_analysis:
+                return ing.central_wf_analysis
+            return ing.lower_wf_analysis
+        except models.Ingredient.DoesNotExist:
+            return None
+
+    def get_max_weight_fraction(self, obj) -> float:
+        try:
+            ing = obj.ingredient
+            if not ing.lower_wf_analysis and not ing.upper_wf_analysis:
+                return ing.central_wf_analysis
+            return ing.upper_wf_analysis
+        except models.Ingredient.DoesNotExist:
+            return None
+
+    class Meta:
+        model = models.RawChem
+        fields = [
+            "id",
+            "sid",
+            "rid",
+            "name",
+            "cas",
+            "qa",
+            "min_weight_fraction",
+            "max_weight_fraction",
+            "data_type",
+            "source",
+        ]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        source="title", help_text="the name of this product", read_only=True
+    )
+    pucs = PUCSerializer(many=True, read_only=True)
+    chemicals = IngredientSerializer(source="rawchems", many=True, read_only=True)
+
+    class Meta:
+        model = models.Product
+        fields = ["id", "name", "pucs", "chemicals"]
+        depth = 1
