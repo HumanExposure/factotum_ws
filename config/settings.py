894df1f00e00ccf7bb92a5a82285e9244da29080
Matt Frost <mwfrost@fastmail.fm>
2019-12-05 07:23:04 -0800

    change logging declaration

--- config/settings.py
+++ config/settings.py
@@ -110,26 +110,27 @@ SWAGGER_SETTINGS = {
     "SECURITY_DEFINITIONS": {},
 }
 
-LOGGING = {
-    "version": 1,
-    "handlers": {
-        "logstash": {
-            "level": "DEBUG",
-            "class": "logstash.TCPLogstashHandler",
-            "host": "localhost",
-            "port": 5959,  # Default value: 5959
-            "version": 1,  # Version of logstash event schema. Default value: 0 (for backward compatibility of the library)
-            "message_type": "django",  # 'type' field in logstash message. Default value: 'logstash'.
-            "fqdn": False,  # Fully qualified domain name. Default value: false.
-            "tags": ["django.request"],  # list of tags. Default: None.
-        }
-    },
-    "loggers": {
-        "django.request": {
-            "handlers": ["logstash"],
-            "level": "DEBUG",
-            "propagate": True,
-        }
-    },
-}
-
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "handlers": {
+            "logstash": {
+                "level": "INFO",
+                "class": "logstash.TCPLogstashHandler",
+                "host": "localhost",
+                "port": 5959,  # Default value: 5959
+                "version": 1,  # Version of logstash event schema. Default value: 0 (for backward compatibility of the library)
+                "message_type": "django",  # 'type' field in logstash message. Default value: 'logstash'.
+                "fqdn": False,  # Fully qualified domain name. Default value: false.
+                "tags": ["django.request"],  # list of tags. Default: None.
+            }
+        },
+        "loggers": {
+            "django.request": {
+                "handlers": ["logstash"],
+                "level": "INFO",
+                "propagate": True,
+            }
+        },
+    }
+)
