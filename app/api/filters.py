0d18082e777882868a1af0e83eb296356dc99f8f
Matt Frost <mwfrost@fastmail.fm>
2019-12-05 18:03:27 -0800

    Add a filter for curated/uncurated

--- app/api/filters.py
+++ app/api/filters.py
@@ -2,6 +2,7 @@ from django.db.models import Prefetch
 from django_filters import rest_framework as filters
 
 from dashboard import models
+from django.db.models import Q
 
 
 class PUCFilter(filters.FilterSet):
@@ -61,6 +62,30 @@ class ChemicalFilter(filters.FilterSet):
     sid = filters.CharFilter(
         "dsstox__sid", help_text="A DTXSID to filter curated chemicals against."
     )
+
+    curated = filters.BooleanFilter(
+        field_name="dsstox",
+        method="filter_curated",
+        help_text="A boolean that indicates whether the search should only return curated or uncurated chemicals",
+    )
+
+    def filter_curated(self, queryset, name, value):
+        if value == True:
+            return queryset.filter(dsstox__isnull=False)
+        if value == False:
+            return queryset.filter(dsstox__isnull=True)
+        else:
+            return queryset
+
+    cas = filters.CharFilter(
+        method="either_cas_filter",
+        help_text="A CAS string that will be compared to both raw and true CAS.",
+    )
+
+    def either_cas_filter(self, queryset, name, value):
+        return models.RawChem.objects.filter(
+            Q(raw_cas=value) | Q(dsstox__true_cas=value)
+        )
 
     class Meta:
         model = models.RawChem
