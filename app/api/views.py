9a1df1c396a01075915b3b7f88ab118e30a7e22e
Matt Covalt <mcovalt@mailbox.org>
2020-01-22 10:02:17 -0500

    Revise the Chemicals API

--- app/api/views.py
+++ app/api/views.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets
 
 from app.api import filters, serializers
 from dashboard import models
-import collections
 
 
 class PUCViewSet(viewsets.ReadOnlyModelViewSet):
@@ -35,85 +34,15 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
 
 class ChemicalViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    Returns chemical records
-
-    list: Returns a list of all the chemical records, filtered by the
-    query parameters, along with any available curated attributes.
-
-    read: Returns a single chemical record specified by an RID
-
-    Args:
-        puc: a PUC ID to select
-        sid: a curated DSSTOX SID
-        curated: a boolean value indicating wheter to return only curated or
-                only uncurated records
-        cas: a string that will be matched (exactly) to either the true CAS or
-                the raw CAS
+    list: Service providing a list of all registered chemical
+    substances linked to data in ChemExpoDB. All chemical data in
+    ChemExpoDB is linked by the DTXSID, or the unique structure based
+    identifier for the chemical substance. Service provides the DTXSID,
+    preferred chemical name, and preferred CAS.
     """
 
-    lookup_field = "rid"
+    lookup_field = "sid"
+    lookup_url_kwarg = "id"
     serializer_class = serializers.ChemicalSerializer
-    queryset = models.RawChem.objects.all().order_by("id")
+    queryset = models.DSSToxLookup.objects.all().order_by("sid")
     filterset_class = filters.ChemicalFilter
-
-
-class ChemicalDistinctAttributeViewSet(viewsets.ReadOnlyModelViewSet):
-
-    """
-    The /chemicals/distinct/{attribute}/ endpoint returns the distinct
-    values for {attribute}. Currently supported attributes are:
-
-    #### sid
-    `/chemicals/distinct/sid/`
-    ```
-    "data":[{"sid":"DTXSID1020273"},{"sid":"DTXSID2021781"}...]
-    ```
-
-    #### true_cas
-    `/chemicals/distinct/true_cas/`
-    ```
-    "data":[{"true_cas":"120-47-8"},{"true_cas":"64-17-5"}...]
-    ```
-
-    #### true_chemname
-    `/chemicals/distinct/true_chemname/`
-    ```
-    "data":[{"true_chemname":"bisphenol a"},{"true_chemname":"chlorine"}...]
-    ```
-
-    """
-
-    Attr = collections.namedtuple("Attr", "query serializer")
-    flds = {
-        "sid": Attr(
-            query="dsstox__sid", serializer=serializers.ChemicalSidAggSerializer
-        ),
-        "true_cas": Attr(
-            query="dsstox__true_cas",
-            serializer=serializers.ChemicalTrueCasAggSerializer,
-        ),
-        "true_chem_name": Attr(
-            query="dsstox__true_chemname",
-            serializer=serializers.ChemicalTrueChemNameAggSerializer,
-        ),
-        "true_chemname": Attr(
-            query="dsstox__true_chemname",
-            serializer=serializers.ChemicalTrueChemNameAggSerializer,
-        ),
-    }
-
-    def get_serializer_class(self):
-        q = self.kwargs.get("attribute", "")
-        attr = self.flds.get(q.lower())
-        if attr:
-            return attr.serializer
-        return serializers.ChemicalSerializer
-
-    def get_queryset(self):
-        q = self.kwargs.get("attribute", "")
-        attr = self.flds.get(q.lower())
-        qs = models.RawChem.objects.filter(dsstox__isnull=False)
-        if attr:
-            return qs.values(attr.query).distinct().order_by(attr.query)
-        else:
-            return qs
