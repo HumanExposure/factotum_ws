d40450cd670e867bafb7b18cb3f406e14551d998
Matt Covalt <mcovalt@mailbox.org>
2020-01-28 17:09:20 -0500

    Rewrite ExtractedChemicalSerializer

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -227,6 +227,63 @@ class ProductSerializer(serializers.ModelSerializer):
         }
 
 
+class ExtractedChemicalSerializer(serializers.ModelSerializer):
+
+    chemical_id = serializers.CharField(
+        label="DTXSID",
+        help_text="The DSSTox Substance Identifier for each chemical included on the document. \
+            May be >1 per document. See the chemicals API for additional information on the \
+            chemical substance.",
+        source="dsstox.sid",
+    )
+
+    class Meta:
+        model = models.ExtractedChemical
+        fields = [
+            "chemical_id",
+            "component",
+            "lower_weight_fraction",
+            "central_weight_fraction",
+            "upper_weight_fraction",
+            "ingredient_rank",
+        ]
+        extra_kwargs = {
+            "component": {
+                "label": "Component",
+                "help_text": "Subcategory grouping chemical information on the document (may \
+                    or may not be populated). Used when the document provides information on \
+                    chemical make-up of multiple components or portions of a product (e.g. a \
+                    hair care set (product) which contains a bottle of shampoo (component 1) \
+                    and bottle of body wash (component 2)).",
+            },
+            "lower_weight_fraction": {
+                "label": "Weight fraction - lower",
+                "help_text": "Lower bound of weight fraction for the chemical substance in the \
+                    product, if provided on the document. If weight fraction is provided as a range, \
+                    lower and upper values are populated. Values range from 0-1.",
+                "source": "lower_wf_analysis",
+            },
+            "central_weight_fraction": {
+                "label": "Weight fraction - central",
+                "help_text": "Central value for weight fraction for the chemical substance in the \
+                    product, if provided on the document. If weight fraction is provided as a point \
+                    estimate, the central value is populated. Values range from 0-1.",
+                "source": "central_wf_analysis",
+            },
+            "upper_weight_fraction": {
+                "label": "Weight fraction - upper",
+                "help_text": "Upper bound of weight fraction for the chemical substance in the product,\
+                 if provided on the document. If weight fraction is provided as a range, lower and \
+                 upper values are populated. Values range from 0-1.",
+                "source": "lower_wf_analysis",
+            },
+            "ingredient_rank": {
+                "label": "Ingredient rank",
+                "help_text": "Rank of the chemical in the ingredient list or document.",
+            },
+        }
+
+
 class DocumentSerializer(serializers.ModelSerializer):
     date = serializers.SerializerMethodField(
         default=None,
@@ -267,44 +324,9 @@ class DocumentSerializer(serializers.ModelSerializer):
              See the Products API for additional information on the product.",
     )
 
-    chemicals = serializers.SerializerMethodField(
-        help_text="""A collection of extracted chemicals related to the document. \
-        The chemical attributes include:<br>
-        *Component `component`:* Subcategory grouping chemical information on the document (may or \
-            may not be populated). Used when the document provides information on chemical \
-                make-up of multiple components or portions of a product (e.g. a hair care \
-                    set (product) which contains a bottle of shampoo (component 1) and \
-                        bottle of body wash (component 2))<br>
-        *DTXSID `sid`:* The DSSTox Substance Identifier for each chemical included on the document. \
-            May be >1 per document. See the chemicals API for additional information on the \
-                chemical substance.<br>
-        *Weight fraction - lower `lower_wf_analysis`:* Lower bound of weight fraction for the chemical substance \
-            in the product, if provided on the document. If weight fraction is provided as a \
-            range, lower and upper values are populated. Values range from 0-1.<br>
-        *Weight fraction - central `central_wf_analysis`:* Central value for weight fraction for the chemical substance \
-            in the product, if provided on the document. If weight fraction is provided as a \
-            point estimate, the central value is populated. Values range from 0-1.<br>
-        *Weight fraction - upper `upper_wf_analysis`:* Upper bound of weight fraction for the chemical substance \
-            in the product, if provided on the document. If weight fraction is provided as a range, \
-                lower and upper values are populated. Values range from 0-1.<br>
-        *Ingredient rank `ingredient_rank`:* Rank of the chemical in the ingredient list or document.
-            """,
-        label="Chemicals",
-        read_only=True,
+    chemicals = ExtractedChemicalSerializer(
+        label="Chemicals", many=True, read_only=True
     )
-
-    def get_chemicals(self, obj):
-        if obj.is_extracted:
-            return ExtractedChemicalSerializer(
-                obj.extractedtext.rawchem.select_subclasses(
-                    "extractedchemical"
-                ).order_by(
-                    "component"
-                ),  # not possible to sort by ingredient_rank, since the subclass is undetermined
-                many=True,
-            ).data
-        else:
-            return None
 
     class Meta:
         model = models.DataDocument
