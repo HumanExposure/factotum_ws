2bd386ffad4e35d0a9fcde498515381d0561505d
Matt Covalt <mcovalt@mailbox.org>
2020-02-05 16:32:18 -0500

    Revert "Merge branch 'master' into dev" (#50)
    This reverts commit 02285a4c0d27b98e0fc7bfc034b29aea5f689095, reversing
    changes made to a3cafbdb6e7722992e2f15245783e4dd1febffdf.

--- app/api/filters.py
+++ app/api/filters.py
@@ -1,7 +1,6 @@
 from django_filters import rest_framework as filters
 
 from dashboard import models
-from django.db.models import Q
 
 
 class PUCFilter(filters.FilterSet):
@@ -36,48 +35,11 @@ class ProductFilter(filters.FilterSet):
 
 
 class ChemicalFilter(filters.FilterSet):
-    """A set of filters that can be applied to the ChemicalSerializer
-
-    Args:
-        filters ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-
     puc = filters.NumberFilter(
         help_text="A PUC ID to filter chemicals against.",
         field_name="curated_chemical__extracted_text__data_document__product__puc__id",
         initial="1",
     )
-
-    sid = filters.CharFilter(
-        "dsstox__sid", help_text="A DTXSID to filter curated chemicals against."
-    )
-
-    curated = filters.BooleanFilter(
-        field_name="dsstox",
-        method="filter_curated",
-        help_text="A boolean that indicates whether the search should only return curated or uncurated chemicals",
-    )
-
-    def filter_curated(self, queryset, name, value):
-        if value == True:
-            return queryset.filter(dsstox__isnull=False)
-        if value == False:
-            return queryset.filter(dsstox__isnull=True)
-        else:
-            return queryset
-
-    cas = filters.CharFilter(
-        method="either_cas_filter",
-        help_text="A CAS string that will be compared to both raw and true CAS.",
-    )
-
-    def either_cas_filter(self, queryset, name, value):
-        return models.RawChem.objects.filter(
-            Q(raw_cas=value) | Q(dsstox__true_cas=value)
-        )
 
     class Meta:
         model = models.DSSToxLookup
