a3cafbdb6e7722992e2f15245783e4dd1febffdf
mwfrost <mwfrost@gmail.com>
2020-02-05 16:02:41 -0500

    1319 Documents api (#47)
    * Trying to traverse relationships from data document to chemical subclasess
    
    * SerializerMethodField and custom serializer working
    
    * Add long docstring for chemicals
    
    * Update test to reflect sort order
    
    * Fix serializers, add test
    
    * Attempted use of extra_kwards for reference and possible future improvement
    
    * Lint
    
    * Rewrite ExtractedChemicalSerializer
    
    * Remove unused serializers
    
    * Fix typos
    
    * Install django_mysql
    
    * Refactor view to use less SQL calls
    
    * Fix test
    
    * Fix data_type & document_type error
    
    * Add test to monitor number of SQL queries
    
    * Add to selet_related on DocumentViewSet queryset to limit SQL calls
    
    * Add test for document_type field
    
    Co-authored-by: Matt Covalt <mcovalt@mailbox.org>

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from dashboard import models
 
 
@@ -119,6 +118,149 @@ class ProductSerializer(serializers.ModelSerializer):
                 "label": "Brand",
                 "source": "brand_name",
                 "help_text": "Brand name for the product, if known. May be the same as the manufacturer.",
+            },
+        }
+
+
+class ExtractedChemicalSerializer(serializers.ModelSerializer):
+
+    chemical_id = serializers.CharField(
+        label="DTXSID",
+        help_text="The DSSTox Substance Identifier for each chemical included on the document. \
+            May be >1 per document. See the chemicals API for additional information on the \
+            chemical substance.",
+        source="dsstox.sid",
+    )
+
+    class Meta:
+        model = models.ExtractedChemical
+        fields = [
+            "chemical_id",
+            "component",
+            "lower_weight_fraction",
+            "central_weight_fraction",
+            "upper_weight_fraction",
+            "ingredient_rank",
+        ]
+        extra_kwargs = {
+            "component": {
+                "label": "Component",
+                "help_text": "Subcategory grouping chemical information on the document (may \
+                    or may not be populated). Used when the document provides information on \
+                    chemical make-up of multiple components or portions of a product (e.g. a \
+                    hair care set (product) which contains a bottle of shampoo (component 1) \
+                    and bottle of body wash (component 2)).",
+            },
+            "lower_weight_fraction": {
+                "label": "Weight fraction - lower",
+                "help_text": "Lower bound of weight fraction for the chemical substance in the \
+                    product, if provided on the document. If weight fraction is provided as a range, \
+                    lower and upper values are populated. Values range from 0-1.",
+                "source": "lower_wf_analysis",
+            },
+            "central_weight_fraction": {
+                "label": "Weight fraction - central",
+                "help_text": "Central value for weight fraction for the chemical substance in the \
+                    product, if provided on the document. If weight fraction is provided as a point \
+                    estimate, the central value is populated. Values range from 0-1.",
+                "source": "central_wf_analysis",
+            },
+            "upper_weight_fraction": {
+                "label": "Weight fraction - upper",
+                "help_text": "Upper bound of weight fraction for the chemical substance in the product,\
+                 if provided on the document. If weight fraction is provided as a range, lower and \
+                 upper values are populated. Values range from 0-1.",
+                "source": "lower_wf_analysis",
+            },
+            "ingredient_rank": {
+                "label": "Ingredient rank",
+                "help_text": "Rank of the chemical in the ingredient list or document.",
+            },
+        }
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    date = serializers.CharField(
+        default=None,
+        read_only=True,
+        allow_null=True,
+        max_length=25,
+        source="extractedtext.doc_date",
+        label="Date",
+        help_text="Publication date for the document.",
+    )
+
+    data_type = serializers.CharField(
+        source="data_group.group_type.title",
+        read_only=True,
+        allow_null=False,
+        label="Data type",
+        help_text="Type of data provided by the document, e.g. 'Composition' \
+            indicates the document provides data on chemical composition of a consumer product.",
+    )
+    document_type = serializers.CharField(
+        source="document_type.title",
+        read_only=True,
+        allow_null=False,
+        label="Document type",
+        help_text="Standardized description of the type of document (e.g. Safety Data Sheet (SDS), \
+            product label, journal article, government report).",
+    )
+    url = serializers.URLField(
+        source="pdf_url",
+        read_only=True,
+        allow_null=True,
+        label="URL",
+        help_text="Link to a locally stored copy of the document.",
+    )
+    products = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True,
+        label="Product IDs",
+        help_text="Unique numeric identifiers for products associated with the \
+             original data document. May be >1 product associated with each document. \
+             See the Products API for additional information on the product.",
+    )
+
+    chemicals = ExtractedChemicalSerializer(
+        label="Chemicals", many=True, read_only=True
+    )
+
+    class Meta:
+        model = models.DataDocument
+        fields = [
+            "id",
+            "title",
+            "subtitle",
+            "organization",
+            "date",
+            "data_type",
+            "document_type",
+            "url",
+            "notes",
+            "products",
+            "chemicals",
+        ]
+        extra_kwargs = {
+            "id": {
+                "label": "Document ID",
+                "help_text": "The unique numeric identifier for the original data document providing data to ChemExpoDB.",
+            },
+            "title": {"label": "Title", "help_text": "Title of the document."},
+            "subtitle": {
+                "label": "Subtitle",
+                "help_text": "Subtitle for the document. \
+                May also be the heading/caption for the table from which data was extracted.",
+            },
+            "organization": {
+                "label": "Organization",
+                "help_text": "The organization which published the document. If the document is \
+                    a peer-reviewed journal article, the name of the journal.",
+            },
+            "notes": {
+                "label": "Notes",
+                "source": "note",
+                "help_text": "General notes about the data document, written by ChemExpoDB data curators.",
             },
         }
 
