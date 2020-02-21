f1ad8ce6033deab338b2e04acb2f2f76b2630209
mwfrost <mwfrost@gmail.com>
2020-02-21 09:33:52 -0500

    1366 factotum_ws PR (#52)
    * Fix document URL for new "DataDocument.file" location
    
    * More robust URL resolver
    
    Even if the file doesn't exist, the API should point to the correct
    location. The media server should issue a 404 if the file is not found.
    
    * Add "MEDIA_URL" environment variable
    
    * Resolve MEDIA_URL from environment in document endpoint
    
    * Remove SerializerMethodField for Django encapsulated logic
    
    * Revert "Remove SerializerMethodField for Django encapsulated logic"
    
    This reverts commit 57be1842abe0b8df8f2f1e4535480a70b2c5d035. It is
    required when files are not associated with a document.
    
    * Add test for documents endpoint
    
    * remove unused function
    
    * Fix linting errors
    
    Co-authored-by: Matt Covalt <mcovalt@mailbox.org>

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -206,8 +206,7 @@ class DocumentSerializer(serializers.ModelSerializer):
         help_text="Standardized description of the type of document (e.g. Safety Data Sheet (SDS), \
             product label, journal article, government report).",
     )
-    url = serializers.URLField(
-        source="pdf_url",
+    url = serializers.SerializerMethodField(
         read_only=True,
         allow_null=True,
         label="URL",
@@ -225,6 +224,9 @@ class DocumentSerializer(serializers.ModelSerializer):
     chemicals = ExtractedChemicalSerializer(
         label="Chemicals", many=True, read_only=True
     )
+
+    def get_url(self, obj) -> serializers.URLField:
+        return obj.file.url if obj.file else None
 
     class Meta:
         model = models.DataDocument
